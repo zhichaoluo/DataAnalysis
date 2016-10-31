da4ab3367143f56268fa6d408dd81b958cf07512
zhichaoluo <zhichao.luo@gmail.com>
2016-10-31 11:03:59 +0800

    exercise to practise vetor manipulation

---
+++ exercise_vector.R
@@ -0,0 +1,9 @@
+#Create a vector with some of your friend's names
+#Get the length of above vector
+#Get the first two friends from above vector
+#Get the 2nd and 3rd friends
+#Sort your friends by names using 2 methods
+# Reverse direction of the above sort	
+#Create with rep and seq: 'a','a',1,2,3,4,5,7,9,11
+#Remove missing value from c(1, 2, NA, 4)
+
